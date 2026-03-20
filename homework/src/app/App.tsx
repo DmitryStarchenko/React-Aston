348705dde17149cf968c36fe6d9fdd6baa3f509d
Dmitry <Dololob92@gmail.com>
2026-03-20 16:18:37 +0300

    feat: added post route

--- homework/src/app/App.tsx
+++ homework/src/app/App.tsx
@@ -1,11 +1,14 @@
-import { MainLayout } from '../shared/layouts/MainLayout';
+import { BrowserRouter } from 'react-router';
 import { ThemeProvider } from '../shared/lib/theme/ThemeProvider';
+import { Router } from './provider/router/Router';
 
 function App() {
   return (
-    <ThemeProvider>
-      <MainLayout />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
