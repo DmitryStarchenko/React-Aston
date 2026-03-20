f74819a320f244250fe3fcab39d1d675254f3107
Dmitry <Dololob92@gmail.com>
2026-03-20 16:42:34 +0300

    feat: added user, album routes

--- homework/src/app/provider/router/Router.tsx
+++ homework/src/app/provider/router/Router.tsx
@@ -1,6 +1,10 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Posts } from '../../../pages/posts/Posts';
 import { NotFound } from '../../../pages/notFound/NotFound';
+import { Users } from '../../../pages/users/Users';
+import { Albums } from '../../../pages/albums/Albums';
+import { Todos } from '../../../pages/todos/Todos';
+import { Photos } from '../../../pages/photos/Photos';
 
 export const Router = () => {
   return (
@@ -11,6 +15,18 @@ export const Router = () => {
         <Route path=":id" element={<Posts />}>
           <Route path="post" element={<Posts />} />
         </Route>
+      </Route>
+      <Route path="users">
+        <Route index element={<Users />} />
+        <Route path=":id" element={<Users />} />
+        <Route path=":id/albums" element={<Albums />} />
+        <Route path=":id/todos" element={<Todos />} />
+        <Route path=":id/posts" element={<Posts />} />
+      </Route>
+      <Route path="albums">
+        <Route index element={<Albums />} />
+        <Route path=":id" element={<Albums />} />
+        <Route path=":id/photos" element={<Photos />} />
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
