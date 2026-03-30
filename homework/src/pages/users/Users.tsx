82a1931dd9e9e92a80efd41554485327b359863c
Dmitry <Dololob92@gmail.com>
2026-03-30 11:29:06 +0300

    styles: changed NavLink styles on the users and albums pages

--- homework/src/pages/users/Users.tsx
+++ homework/src/pages/users/Users.tsx
@@ -2,17 +2,20 @@ import { NavLink } from 'react-router-dom';
 import styles from './Users.module.css';
 
 export const Users = () => {
+  const getLinkClass = ({ isActive }: { isActive: boolean }) =>
+    isActive ? `${styles.link} ${styles.active}` : styles.link;
+
   return (
     <div className={styles.content}>
       <h1>Users</h1>
       <div className={styles.nav}>
-        <NavLink className={styles.link} to="1/albums">
+        <NavLink className={getLinkClass} to="1/albums">
           Albums
         </NavLink>
-        <NavLink className={styles.link} to="5/todos">
+        <NavLink className={getLinkClass} to="5/todos">
           Todos
         </NavLink>
-        <NavLink className={styles.link} to="8/posts">
+        <NavLink className={getLinkClass} to="8/posts">
           Posts
         </NavLink>
       </div>
